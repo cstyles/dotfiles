ef95c95f721ed2f1c667c288128cb064eba63adc
Collin Styles <collingstyles@gmail.com>
2018-09-28 15:10:46 -0700

    setup.sh: Fixed command for macOS systems

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 cd $(dirname "$0")
-stow --target "$HOME" $(find -mindepth 1 -maxdepth 1 -type d -not -path ./.git | cut -b 3-)
+stow --target "$HOME" $(find . -mindepth 1 -maxdepth 1 -type d -not -path ./.git | cut -b 3-)
