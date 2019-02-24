7370c670591b74f1a3f5142dffd96ea8ebd048a1
Collin Styles <collingstyles@gmail.com>
2019-02-24 09:41:04 -0800

    fish: Add abbreviations

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,2 +1,7 @@
 set -gx PATH $HOME/.cargo/bin $HOME/.rbenv/shims $PATH
 set -gx EDITOR "vim"
+
+# Abbreviations:
+abbr --global --add g git
+abbr --global --add v vim
+abbr --global --add n nvim
--- fish/.config/fish/functions/v.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function v --description 'alias v vim'
-	vim $argv;
-end
