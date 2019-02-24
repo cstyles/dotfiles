2b7e13b9577c6f9879a36cde4dfeaf482bf369d2
Collin Styles <collingstyles@gmail.com>
2019-02-24 11:25:36 -0800

    rg: Add .ripgreprc

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 set -gx fish_user_paths $HOME/.cargo/bin $HOME/.rbenv/shims ./node_modules/.bin
 set -gx EDITOR "vim"
 set -gx HOMEBREW_EDITOR $EDITOR
+set -gx RIPGREP_CONFIG_PATH $HOME/.ripgreprc
 
 # Abbreviations:
 abbr --global --add g git
--- fish/.config/fish/functions/rg.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function rg --wraps rg --description 'alias rg rg -S'
-	command rg -S --color=always $argv;
-end
