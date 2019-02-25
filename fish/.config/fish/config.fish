a63dc558a18438f5588d883908bee881d9a41c4a
Collin Styles <collingstyles@gmail.com>
2019-02-24 17:11:59 -0800

    fish: Set PAGER to `less`

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
 set -gx fish_user_paths $HOME/.cargo/bin $HOME/.rbenv/shims ./node_modules/.bin
 set -gx EDITOR "vim"
+set -gx PAGER "less"
 set -gx HOMEBREW_EDITOR $EDITOR
 set -gx RIPGREP_CONFIG_PATH $HOME/.ripgreprc
 
