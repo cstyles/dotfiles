59c50702b26ba6ea52cf8528152161ad2ccdc034
Collin Styles <collingstyles@gmail.com>
2019-02-24 20:03:59 -0800

    fish: Add a binding to append " | rg" to the end of the line

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,8 +3,12 @@ set -gx EDITOR "vim"
 set -gx PAGER "less"
 set -gx HOMEBREW_EDITOR $EDITOR
 set -gx RIPGREP_CONFIG_PATH $HOME/.ripgreprc
+set -gx fish_grep rg
 
 # Abbreviations:
 abbr --global --add g git
 abbr --global --add v vim
 abbr --global --add n nvim
+
+# Bindings:
+bind \er __fish_grep_pipe
---
+++ fish/.config/fish/functions/__fish_grep_pipe.fish
@@ -0,0 +1,25 @@
+function __fish_grep_pipe
+    # Choose a grep-like program to use
+    set -l cmd grep
+    if set -q fish_grep
+        set cmd $fish_grep
+    end
+
+    # If command line is empty, populate it with the last command
+    if test -z (commandline -j)
+        commandline -a $history[1]
+    end
+
+    # Only edit the command if we haven't already
+    if commandline -j | string match -q -r -v "$cmd *\$"
+        # Add a space unless there's already one at the end of the line
+        if commandline -j | string match -q -r -v " \$"
+            commandline --append --current-job " "
+        end
+
+        commandline --append --current-job "2>&1 | $cmd "
+
+        # Go to end of line
+        commandline --cursor 999999
+    end
+end
