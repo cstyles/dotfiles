e20dc47ac0395609469f67d7c19b0018bbe4e98f
Collin Styles <collingstyles@gmail.com>
2019-05-31 14:16:58 -0700

    fish: Remove rgab function
    It turns out there's a -C option that does this. Awkward.

--- fish/.config/fish/functions/rgab.fish
+++
@@ -1,6 +0,0 @@
-# Defined in - @ line 0
-function rgab --wraps rg --description 'alias rg rgab -A 3 -B 3'
-	set distance $argv[1]
-	set -e argv[1]
-	command rg -A "$distance" -B "$distance" $argv;
-end
