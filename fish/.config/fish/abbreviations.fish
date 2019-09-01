991a97cd1b5d3d634bf2f8b4be666badf9514fa6
Collin Styles <collingstyles@gmail.com>
2019-09-01 09:34:28 -0700

    git: New shortcuts + run ctags post checkout

--- fish/.config/fish/abbreviations.fish
+++ fish/.config/fish/abbreviations.fish
@@ -3,6 +3,10 @@ abbr --global --add v vim
 abbr --global --add n nvim
 
 # Git:
+abbr --global --add gap git add --patch
+abbr --global --add gb git branch
+abbr --global --add gbr git branch
+abbr --global --add gbrv git branch --verbose
 abbr --global --add gca git commit --amend
 abbr --global --add gci git commit
 abbr --global --add gcb git checkout -b
@@ -15,9 +19,14 @@ abbr --global --add gg git graph
 abbr --global --add gl git log
 abbr --global --add gm git merge
 abbr --global --add gp git push
+abbr --global --add gra git rebase --abort
+abbr --global --add grc git rebase --continue
+abbr --global --add gri git rebase --interactive
+abbr --global --add grs git rebase --skip
 abbr --global --add gre git remote
 abbr --global --add grv git remote --verbose
 abbr --global --add gs git show
+abbr --global --add gsc git switch --create
 abbr --global --add gsl git stash list
 abbr --global --add gss git stash show -p
 abbr --global --add gst git status
