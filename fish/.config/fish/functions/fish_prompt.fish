e203cfcad2ac6b245b479b9c86afb98164d10f2a
Collin Styles <collingstyles@gmail.com>
2019-10-11 16:30:10 -0700

    fish: Update fish_prompt
    - Add git_prompt
    - Format so it's easier to see which %s corresponds to which variable

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,12 +1,31 @@
 function fish_prompt --description 'Write out the prompt'
-	set -l home_escaped (echo -n $HOME | sed 's/\//\\\\\//g')
-    set -l pwd (echo -n $PWD | sed "s/^$home_escaped/~/" | sed 's/ /%20/g')
-    set -l prompt_symbol ''
     switch "$USER"
         case root toor
             set prompt_symbol '#'
         case '*'
             set prompt_symbol '$'
     end
-    printf "[%s%s%s@%s%s %s%s%s] %s%s %s" (set_color --bold cyan) $USER (set_color normal) (set_color --bold green) (prompt_hostname) (set_color brblue) $pwd (set_color normal) (set_color brred) $prompt_symbol (set_color normal)
+
+    set git_prompt (__fish_git_prompt)
+
+    # TODO: root user should be colored red
+
+    # NOTE: The ""s around the variables are important because if the variable
+    # is empty, it will evaluate to an empty string and be interpolated
+    # correctly. Without the ""s, it will evaluate to nothing and throw off
+    # the alignment of the interpolation (i.e., all the subsequent '%s's will
+    # be shifted over by one.
+    printf "[%s%s%s@%s%s %s%s%s%s] %s%s %s" \
+             (set_color --bold cyan) \
+               "$USER" \
+                 (set_color normal) \
+                    (set_color --bold green) \
+                      (prompt_hostname) \
+                         (set_color brblue) \
+                           (prompt_pwd) \
+                             (set_color normal) \
+                               "$git_prompt" \
+                                   (set_color brred) \
+                                     "$prompt_symbol" \
+                                        (set_color normal)
 end
