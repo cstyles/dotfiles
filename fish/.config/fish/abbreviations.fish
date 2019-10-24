470661f5bfdc386a150d8296e063086a31df7905
Collin Styles <collingstyles@gmail.com>
2019-10-24 09:22:40 -0700

    fish: Add a bunch of git abbreviations

--- fish/.config/fish/abbreviations.fish
+++ fish/.config/fish/abbreviations.fish
@@ -4,6 +4,8 @@ abbr --global --add n nvim
 abbr --global --add be bundle exec
 
 # Git:
+abbr --global --add ga git add
+abbr --global --add gab git absorb
 abbr --global --add gap git add --patch
 abbr --global --add gb git branch
 abbr --global --add gbr git branch
@@ -20,13 +22,19 @@ abbr --global --add gdc git diff --cached
 abbr --global --add gdhu git diff HEAD "'@{u}'"
 abbr --global --add gduh git diff "'@{u}'" HEAD
 abbr --global --add gf git fetch
+abbr --global --add gfa git fetch --all
 abbr --global --add gg git graph
 abbr --global --add gl git log
+abbr --global --add glo git log --oneline
 abbr --global --add glpr git log --patch --reverse
 abbr --global --add gm git merge
 abbr --global --add gp git push
+abbr --global --add gpl git pull
+abbr --global --add gr git reset
 abbr --global --add gra git rebase --abort
+abbr --global --add grb git rebase
 abbr --global --add grc git rebase --continue
+abbr --global --add grh git reset --hard
 abbr --global --add gri git rebase --interactive
 abbr --global --add grs git rebase --skip
 abbr --global --add gre git remote
@@ -36,4 +44,6 @@ abbr --global --add gsc git switch --create
 abbr --global --add gsl git stash list
 abbr --global --add gss git stash show -p
 abbr --global --add gst git status
-abbr --global --add gwd diff --word-diff
+abbr --global --add gsw git switch
+abbr --global --add gswd git show --word-diff
+abbr --global --add gwd git diff --word-diff
