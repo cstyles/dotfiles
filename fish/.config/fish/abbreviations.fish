f2db45640de7ba93011e44953b5396d46bfef3e2
Collin Styles <collingstyles@gmail.com>
2019-11-25 12:27:43 -0800

    fish: Add a bunch of abbreviations

--- fish/.config/fish/abbreviations.fish
+++ fish/.config/fish/abbreviations.fish
@@ -7,6 +7,7 @@ abbr --global --add be bundle exec
 # Git:
 abbr --global --add ga git add
 abbr --global --add gab git absorb
+abbr --global --add gabn git absorb --verbose --dry-run
 abbr --global --add gap git add --patch
 abbr --global --add gb git branch
 abbr --global --add gbr git branch
@@ -27,6 +28,7 @@ abbr --global --add gfa git fetch --all
 abbr --global --add gg git graph
 abbr --global --add gl git log
 abbr --global --add glo git log --oneline
+abbr --global --add glp git log --patch
 abbr --global --add glpr git log --patch --reverse
 abbr --global --add gm git merge
 abbr --global --add gp git push
@@ -35,10 +37,11 @@ abbr --global --add gr git reset
 abbr --global --add gra git rebase --abort
 abbr --global --add grb git rebase
 abbr --global --add grc git rebase --continue
+abbr --global --add gre git restore
+abbr --global --add grem git remote
 abbr --global --add grh git reset --hard
 abbr --global --add gri git rebase --interactive
 abbr --global --add grs git rebase --skip
-abbr --global --add gre git remote
 abbr --global --add grv git remote --verbose
 abbr --global --add gs git show
 abbr --global --add gsc git switch --create
@@ -48,3 +51,4 @@ abbr --global --add gst git status
 abbr --global --add gsw git switch
 abbr --global --add gswd git show --word-diff
 abbr --global --add gwd git diff --word-diff
+abbr --global --add gwt git worktree
