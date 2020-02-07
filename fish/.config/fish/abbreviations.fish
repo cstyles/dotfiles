384d53dd85925cbd20f1fa1c34e74768782b0fe6
Collin Styles <collingstyles@gmail.com>
2020-02-07 13:08:58 -0800

    fish: Add a bunch more git abbreviations

--- fish/.config/fish/abbreviations.fish
+++ fish/.config/fish/abbreviations.fish
@@ -10,17 +10,21 @@ abbr --global --add dushcsh 'du -shc * | sort -h'
 abbr --global --add ga git add
 abbr --global --add gab git absorb
 abbr --global --add gabn git absorb --verbose --dry-run
+abbr --global --add gabr git absorb --verbose --and-rebase
 abbr --global --add gap git add --patch
 abbr --global --add gb git branch
 abbr --global --add gbr git branch
+abbr --global --add gbrsu git branch --set-upstream-to origin/\(git rev-parse --abbrev-ref HEAD\)
 abbr --global --add gbrv git branch --verbose
 abbr --global --add gca git commit --amend
-abbr --global --add gcaa git commit --amend --no-edit
+abbr --global --add gcan git commit --amend --no-edit
 abbr --global --add gci git commit
 abbr --global --add gcb git checkout -b
 abbr --global --add gcf git commit --fixup
+abbr --global --add gcfg git config --edit
 abbr --global --add gcl git clone
 abbr --global --add gco git checkout
+abbr --global --add gcp git cherry-pick
 abbr --global --add gd git diff
 abbr --global --add gdc git diff --cached
 abbr --global --add gdhu git diff HEAD "'@{u}'"
@@ -39,9 +43,12 @@ abbr --global --add gr git reset
 abbr --global --add gra git rebase --abort
 abbr --global --add grb git rebase
 abbr --global --add grc git rebase --continue
+abbr --global --add grd git range-diff
 abbr --global --add gre git restore
 abbr --global --add grem git remote
+abbr --global --add gres git restore --staged
 abbr --global --add grh git reset --hard
+abbr --global --add grhu git reset --hard "'@{u}'"
 abbr --global --add gri git rebase --interactive
 abbr --global --add grs git rebase --skip
 abbr --global --add grv git remote --verbose
@@ -53,4 +60,5 @@ abbr --global --add gst git status
 abbr --global --add gsw git switch
 abbr --global --add gswd git show --word-diff
 abbr --global --add gwd git diff --word-diff
+abbr --global --add gwdc git diff --word-diff --cached
 abbr --global --add gwt git worktree
