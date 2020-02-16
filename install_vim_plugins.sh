e0de154ec0e89739d2fc084b9ea36e0827a40bbd
Collin Styles <collingstyles@gmail.com>
2020-02-15 16:44:35 -0800

    vim: Install my fork of OceanicNext vim plugin/theme

--- install_vim_plugins.sh
+++ install_vim_plugins.sh
@@ -12,7 +12,7 @@ https://github.com/sheerun/vim-polyglot.git
 https://github.com/neomake/neomake
 https://github.com/tpope/vim-fugitive
 https://github.com/junegunn/fzf.vim
-https://github.com/mhartington/oceanic-next.git
+https://github.com/cstyles/oceanic-next.git
 https://github.com/mbbill/undotree.git
 https://github.com/tpope/vim-rhubarb.git
 https://github.com/tpope/vim-unimpaired.git
