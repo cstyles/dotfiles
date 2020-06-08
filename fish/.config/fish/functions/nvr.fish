438f8a046066796fd7dae769739ec3bc41ab6af9
Collin Styles <collingstyles@gmail.com>
2020-06-07 19:09:33 -0700

    fish: Add `-s` option to `nvr` (silence "no server found" message)

--- fish/.config/fish/functions/nvr.fish
+++ fish/.config/fish/functions/nvr.fish
@@ -1,4 +1,4 @@
 function nvr --description 'neovim-remote'
-  command "$NEOVIM_REMOTE_PATH/venv/bin/python3" "$NEOVIM_REMOTE_PATH/nvr/nvr.py" $argv
+  command "$NEOVIM_REMOTE_PATH/venv/bin/python3" "$NEOVIM_REMOTE_PATH/nvr/nvr.py" -s $argv
 end
 
