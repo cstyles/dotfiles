7b085ea50a8330df2f6a325f19afb45d1507163a
Collin Styles <collingstyles@gmail.com>
2020-07-26 11:21:46 -0700

    nvr: Initialize NEOVIM_REMOTE_PATH if unset and remove comments

--- nvr.sh
+++ nvr.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env sh
 
-# echo $@
-# nvim $@
-
-# TODO:
-# [x] Check presence of NVIM_LISTEN_ADDRESS (if not, just use nvim)
-# [ ] What else uses EDITOR? Will this cause problems in other workflows?
-# [ ] Can we re-enter insert/terminal mode after deleting buffer?
-# [x] bufhidden is set on terminal buffer :\
+if [ -z ${NEOVIM_REMOTE_PATH+x} ]; then
+  NEOVIM_REMOTE_PATH="$HOME/dev/python/neovim-remote"
+fi
 
 if [ -z ${NVIM_LISTEN_ADDRESS+x} ]; then
   nvim "$@"
