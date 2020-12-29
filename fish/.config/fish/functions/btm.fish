11399faaa6f6e677b3af48b47d818eec7c644d13
Collin Styles <collingstyles@gmail.com>
2020-12-29 14:44:14 -0800

    fish: Remove `btm` function
    I'm just gonna install it via cargo instead.

--- fish/.config/fish/functions/btm.fish
+++
@@ -1,3 +0,0 @@
-function btm --wraps=/Users/collin.styles/dev/rust/bottom/target/release/btm --description 'alias btm $HOME/dev/rust/bottom/target/release/btm'
-  "$HOME/dev/rust/bottom/target/release/btm" $argv;
-end
