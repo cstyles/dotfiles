11546e53a364006ae6c09d09b0679167e4be350a
Collin Styles <collingstyles@gmail.com>
2021-07-02 21:34:11 -0700

    fish: Remove jisho functions

--- fish/.config/fish/functions/j.fish
+++
@@ -1,3 +0,0 @@
-function j
-	~/dev/anki/venv/bin/python3 $HOME/dev/anki/jisho.py -o $HOME/dev/anki/out.csv $argv
-end
--- fish/.config/fish/functions/jk.fish
+++
@@ -1,3 +0,0 @@
-function jk
-	~/dev/anki/venv/bin/python3 $HOME/dev/anki/jisho.py -o $HOME/dev/anki/out.csv -k $argv
-end
