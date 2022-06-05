8961104b6d627ca0d8e74d567b87e7b04496e236
Collin Styles <collingstyles@gmail.com>
2022-06-05 15:21:18 -0700

    wezterm: Add wezterm configuration

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require 'wezterm';
+
+return {
+  color_scheme = 'Dracula',
+  enable_scroll_bar = true,
+  font = wezterm.font('Hack Nerd Font'),
+  font_size = 14.0,
+
+  keys = {
+    { key='k', mods='CMD', action=wezterm.action{ClearScrollback='ScrollbackAndViewport'} },
+    { key='LeftArrow', mods='CMD|SHIFT', action=wezterm.action{MoveTabRelative=-1} },
+    { key='RightArrow', mods='CMD|SHIFT', action=wezterm.action{MoveTabRelative=1} },
+    -- Alt-Shift-Tab -> iterm2 compatible ESC-CTRL-Y
+    { key='Tab', mods='ALT|SHIFT', action=wezterm.action{SendString='\x1b\x19'} },
+  },
+}
