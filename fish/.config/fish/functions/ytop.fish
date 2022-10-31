44f43140153d4c857a7e86ad4cc96d78948fa82e
Collin Styles <collingstyles@gmail.com>
2022-10-30 18:06:00 -0700

    fish: Remove ytop function
    I use bottom now.

--- fish/.config/fish/functions/ytop.fish
+++
@@ -1,3 +0,0 @@
-function ytop --wraps=/Users/collin.styles/dev/rust/ytop/target/release/ytop --description 'alias ytop $HOME/dev/rust/ytop/target/release/ytop'
-  "$HOME/dev/rust/ytop/target/release/ytop" $argv;
-end
