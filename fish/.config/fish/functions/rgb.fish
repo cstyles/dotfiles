9f4cd02bbdaecc1c43ca9f5f2f255f81fa193992
Collin Styles <collingstyles@gmail.com>
2025-07-17 10:20:55 -0700

    fish: Update path to `rgb` executable
    I'm now installing this to my cargo bin directory so I don't need the
    absolute path anymore. Still leaving the fish wrapper for default args,
    plus the `--wraps` option which gives me completion suggestions.

--- fish/.config/fish/functions/rgb.fish
+++ fish/.config/fish/functions/rgb.fish
@@ -1,3 +1,3 @@
 function rgb --wraps=rg --description 'Display ripgrep results using bat'
-  ~/dev/rust/ripgrep/target/release/rg --bat --context=3 $argv;
+  command rgb --bat --context=3 $argv;
 end
