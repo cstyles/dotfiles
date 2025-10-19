e7d8db115bb45e854e934cf9a91ab050033742ad
Collin Styles <collingstyles@gmail.com>
2025-10-19 16:29:50 -0700

    fish: Add completion for `sentence` case to `heck`

--- fish/.config/fish/completions/heck.fish
+++ fish/.config/fish/completions/heck.fish
@@ -1,1 +1,1 @@
-complete --command heck --no-files -a 'kebab lower_camel pascal shouty_kebab shouty_snake snake title train upper_camel'
+complete --command heck --no-files -a 'kebab lower_camel pascal sentence shouty_kebab shouty_snake snake title train upper_camel'
