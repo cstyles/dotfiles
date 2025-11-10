51b3ffc942a0d914924a806afe1d83e66c2c6725
Collin Styles <collingstyles@gmail.com>
2025-11-09 19:29:57 -0800

    fish: Suggest recent commits when tab-completing `git-split`
    `git-split` now accepts an argument: a prior commit to automatically
    jump to and split (which is more efficient than initiating a rebase,
    breaking after the commit, and then running `git split`).

--- fish/.config/fish/completions.fish
+++ fish/.config/fish/completions.fish
@@ -1,2 +1,3 @@
 complete -f -c git -n '__fish_git_using_command snipe' -ka '(__fish_git_recent_commits)'
+complete -f -c git -n '__fish_git_using_command split' -ka '(__fish_git_recent_commits)'
 complete -f -c git -n '__fish_git_using_command restack' -ka '(__fish_git_branches)'
