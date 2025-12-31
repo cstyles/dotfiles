79419a3f71c4a0ff78984a1b23b9a86f6d8531a0
Collin Styles <collingstyles@gmail.com>
2025-12-30 23:14:59 -0800

    fish: Pipe into `xargs` instead of launching a subshell

--- fish/.config/fish/functions/rgg.fish
+++ fish/.config/fish/functions/rgg.fish
@@ -1,4 +1,4 @@
 function rgg --wraps rg --description "ripgrep but only on files tracked in git"
     # TODO: allow running from non-git root
-    rg $argv (git ls-files)
+    git ls-files -z | xargs -0 rg $argv
 end
