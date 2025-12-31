0b3206ce51204459d2a5fbf8543e8836537580e7
Collin Styles <collingstyles@gmail.com>
2025-12-30 22:52:27 -0800

    fish: Document why custom git completions are where they are

--- fish/.config/fish/completions.fish
+++ fish/.config/fish/completions.fish
@@ -1,3 +1,7 @@
+# These need to be defined here (and not in `completions/git.fish`) because
+# otherwise the `__fish_git_using_command` function won't be available yet,
+# presumably because the main `git` completions are loaded later.
+
 complete -f -c git -n '__fish_git_using_command snipe' -ka '(__fish_git_recent_commits)'
 complete -f -c git -n '__fish_git_using_command split' -ka '(__fish_git_recent_commits)'
 complete -f -c git -n '__fish_git_using_command restack' -ka '(__fish_git_branches)'
