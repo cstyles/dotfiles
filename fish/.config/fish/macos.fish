8602beac9a5a7191e48e6a4cf1876b22a11d8121
Collin Styles <collingstyles@gmail.com>
2025-12-31 00:32:44 -0800

    fish: Move `fzf_key_bindings` setup into macOS-specific file
    This was previously hardcoded for macOS so it wasn't portable. Also
    apparently it's not included by default on macOS like is on Linux so we
    have to explicitly `source` it here.

--- fish/.config/fish/macos.fish
+++ fish/.config/fish/macos.fish
@@ -1,3 +1,6 @@
 set --append fish_user_paths /opt/homebrew/bin /opt/homebrew/sbin /opt/homebrew/opt/git/share/git-core/contrib/git-jump
 
 set -gx HOMEBREW_EDITOR $EDITOR
+
+# Enable fzf integration with fish (e.g., fuzzy find git files, etc.)
+source /opt/homebrew/opt/fzf/shell/key-bindings.fish
