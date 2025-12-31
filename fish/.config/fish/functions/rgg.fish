5b1d5b11455efb94fcb8168cc5d4184f55091748
Collin Styles <collingstyles@gmail.com>
2025-12-30 23:21:05 -0800

    fish: Allow running `rgg` from non-git-root directories
    Previously it would only search for files tracked by git _and_ in the
    current directory. Now it will always act as if it was run from the root
    directory of the git repository.

--- fish/.config/fish/functions/rgg.fish
+++ fish/.config/fish/functions/rgg.fish
@@ -1,4 +1,3 @@
 function rgg --wraps rg --description "ripgrep but only on files tracked in git"
-    # TODO: allow running from non-git root
-    git ls-files -z | xargs -0 rg $argv
+    git -C (git root) ls-files -z | xargs -0 rg $argv
 end
