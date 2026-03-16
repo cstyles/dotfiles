1596cde8e8431589671e186ee0cb4de2b35a3f9c
Collin Styles <collingstyles@gmail.com>
2026-03-15 23:23:37 -0700

    fish: Fix `rgg` command when run in non-git root directories
    In `5b1d5b11455efb94fcb8168cc5d4184f55091748` I edited this function to
    allow searching for files throughout the entire git repository, not just
    the current directory. But now the command is broken because the file
    paths returned by `git ls-files` are relative to the git root, meaning
    that `rg` won't find them if we're not in the git root.
    
    The fix is just to pass the git root to `git ls-files` so that it
    searches from there _and_ outputs paths relative to the current
    directory.

--- fish/.config/fish/functions/rgg.fish
+++ fish/.config/fish/functions/rgg.fish
@@ -1,3 +1,3 @@
 function rgg --wraps rg --description "ripgrep but only on files tracked in git"
-    git -C (git root) ls-files -z | xargs -0 rg $argv
+    git  ls-files -z (git root) | xargs -0 rg $argv
 end
